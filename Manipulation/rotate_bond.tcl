12df855167d551de4554980d41b7cb780353fa6b
Jérôme Hénin <heninj@gmail.com>
2024-02-23 11:16:33 +0100

    More interactive pick-and-rotate

--- Manipulation/rotate_bond.tcl
+++ Manipulation/rotate_bond.tcl
@@ -1,42 +1,68 @@
 # Make a bond between two atoms rotatable using the keyboard or the rotate_group procedure
 
-proc make_bond_rotatable { a1 a2 { selText protein } {molid top} } {
+proc pick_and_rotate { } {
+  global ::vmd_pick_event
+
+  set ::rotate_a1 -1
+  # remove previous trace, if any
+  trace remove variable ::vmd_pick_event write pick_rotate_callback
+  trace add variable ::vmd_pick_event write pick_rotate_callback
+
+  puts "Use mouse to pick two atoms"
+  mouse mode pick
+}
+
+proc pick_rotate_callback {args} {
+  global vmd_pick_atom vmd_pick_mol
+  set a $::vmd_pick_atom
+  if { $::rotate_a1 == -1 } {
+    set ::rotate_a1 $a
+  } else {
+    set a2 $a
+    puts "Rotating around atoms: $::rotate_a1 $a2 in molecule $vmd_pick_mol"
+    make_bond_rotatable $::rotate_a1 $a2 $vmd_pick_mol
+    set ::rotate_a1 -1
+  }
+}
+
+
+proc make_bond_rotatable { a1 a2 {molid top} } {
+
+  if [info exists ::rotate_rep] {
+    set r [mol repindex $molid $::rotate_rep]
+    if { $r > -1 } {
+      mol delrep $r $molid
+    }
+    unset ::rotate_rep
+  }
 
   # First, split the selection into fragments separated by selected bond
   # We will rotate the smaller fragment
 
-  # Use bond distance heuristic to follow molecular graph. We could use VMD's defined bonds instead.
-  set bond_cutoff 1.6
+  set sel [atomselect $molid "same fragment as index $a1 $a2"]
+  set bonds [$sel getbonds]
+
+  set part1 [list $a1]; set cur_part1 $part1
+  set part2 [list $a2]; set cur_part2 $part2
 
   # Extra stopping criterion to avoid infinite loops
-  set max_iter 1000
+  set max_iter 10000
 
-  set n1 0
-  set part1 [atomselect $molid "$selText and not index $a2 and within $bond_cutoff of index $a1"]
-  set n2 0
-  set part2 [atomselect $molid "$selText and not index $a1 and within $bond_cutoff of index $a2"]
-
-  # Recursively discover two halves until either one converges
+  # Recursively discover half until either one converges
   set i 0
-  while { $i < $max_iter && $n1 < [$part1 num] && $n2 < [$part2 num]} {
-    set n1 [$part1 num]
-    set new_part1 [atomselect $molid "$selText and not index $a2 and within $bond_cutoff of index [$part1 list]"]
-    $part1 delete
-    set part1 $new_part1
-
-    set n2 [$part2 num]
-    set new_part2 [atomselect $molid "$selText and not index $a1 and within $bond_cutoff of index [$part2 list]"]
-    $part2 delete
-    set part2 $new_part2
+  while { $i < $max_iter && [llength $cur_part1] > 0 && [llength $cur_part2] > 0 } {
+    lassign [extend_half $part1 $cur_part1 $a2 $bonds] part1 cur_part1
+    lassign [extend_half $part2 $cur_part2 $a1 $bonds] part2 cur_part2
     incr i
   }
+
+  set spart1 [atomselect $molid "index $part1"]
+  set spart2 [atomselect $molid "index $part2"]
+
   # For debugging: set beta field
   # [atomselect $molid all] set beta 0
-  # $part1 set beta 1
-  # $part2 set beta 2
-  # puts "$part1 $part2"
-  # $part1 global
-  # $part2 global
+  # $spart1 set beta 1
+  # $spart2 set beta 2
 
   # Persistent atomselects for individual atoms, used by rotate_group
   set sela1 [atomselect $molid "index $a1"]
@@ -44,19 +70,31 @@ proc make_bond_rotatable { a1 a2 { selText protein } {molid top} } {
   $sela1 global
   $sela2 global
 
+  mol color ColorID 1
+  mol representation Licorice 0.3 12
+
   # First partition to converge is smallest, make that one rotate
-  if { $n1 == [$part1 num] } {
-    $part1 global
-    puts "Done. Use arrow keys to rotate."
-    user add key Left "rotate_group $sela2 $sela1 $part1 -5"
-    user add key Right "rotate_group $sela2 $sela1 $part1 5"
-  } elseif { $n2 == [$part2 num] } {
-    $part2 global
-    puts "Done. Use arrow keys to rotate."
-    user add key Left "rotate_group $sela1 $sela2 $part2 -5"
-    user add key Right "rotate_group $sela1 $sela2 $part2 5"
+  if { [llength $cur_part1] == 0 } {
+    $spart1 global
+    puts "Molecule partitioning done. Use arrow keys to rotate."
+    user add key Left "rotate_group $sela2 $sela1 $spart1 -5"
+    user add key Right "rotate_group $sela2 $sela1 $spart1 5"
+    mol selection "index $part1"
+    mol addrep $molid
+    set repid [expr [molinfo $molid get numreps] - 1]
+    set ::rotate_rep [mol repname $molid $repid]
+
+  } elseif { [llength $cur_part2] == 0 } {
+    $spart2 global
+    puts "Molecule partitioning done. Use arrow keys to rotate."
+    user add key Left "rotate_group $sela1 $sela2 $spart2 -5"
+    user add key Right "rotate_group $sela1 $sela2 $spart2 5"
+    mol selection "index $part2"
+    mol addrep $molid
+    set repid [expr [molinfo $molid get numreps] - 1]
+    set ::rotate_rep [mol repname $molid $repid]
   } else {
-    puts "No convergence after $i iterations"
+    puts "Molecule partitioning did not converge after $i iterations"
   }
 }
 
@@ -64,7 +102,6 @@ proc make_bond_rotatable { a1 a2 { selText protein } {molid top} } {
 # Rotate group $sel by $angle around vector from $sela1 to $sela2
 
 proc rotate_group { sela1 sela2 sel angle } {
-
   set pos1 [lindex [$sela1 get {x y z}] 0]
   set pos2 [lindex [$sela2 get {x y z}] 0]
   set axis [vecnorm [vecsub $pos1 $pos2]]
@@ -72,4 +109,22 @@ proc rotate_group { sela1 sela2 sel angle } {
   $sel moveby [vecinvert $pos2]
   $sel move [transabout $axis $angle]
   $sel moveby $pos2
+}
+
+
+proc extend_half { part1 cur_part1 a2 bonds } {
+  set new_part1 [list]
+  foreach c $cur_part1 {
+    # Iterate over bonded neighbors of recent batch of added atoms
+    foreach a [lindex $bonds $c] {
+      if { $a != $a2 && [lsearch -sorted -integer $part1 $a] == -1 } {
+        lappend new_part1 $a
+      }
+    }
+  }
+  set cur_part1 [lsort -unique -integer $new_part1]
+  # Most efficient concatenation
+  set part1 [lsort -unique -integer [list {*}$part1 {*}$cur_part1]]
+
+  return [list $part1 $cur_part1]
 }
